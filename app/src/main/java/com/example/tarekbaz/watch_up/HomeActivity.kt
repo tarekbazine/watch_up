c607299fe5c2e3e6d4b0e456681efe95554d6b03
mahfoud <mail@esi.com>
2018-03-26 20:18:54 +0100

    film recycler view

--- app/src/main/java/com/example/tarekbaz/watch_up/HomeActivity.kt
+++ app/src/main/java/com/example/tarekbaz/watch_up/HomeActivity.kt
@@ -10,8 +10,28 @@ import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.app_bar_home.*
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.content_home.*
+
 
 class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    //TODO
+    val filmNames: List<String> = mutableListOf(
+            "La Belle et La Bète", "Hunger Game", "Drone" , "Hunger Game 2"
+    )
+
+    val imageFilmsUrls: List<Int> = mutableListOf(
+            R.drawable.film1,R.drawable.film2,R.drawable.film3,R.drawable.film4
+    )
+
+    private fun initRecyclerView() {
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        home_film_slider.setLayoutManager(layoutManager)
+        val adapter = HomeRecyclerViewAdapter(this, filmNames , imageFilmsUrls)
+        home_film_slider.setAdapter(adapter)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +42,10 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
-
         nav_view.setNavigationItemSelectedListener(this)
+
+        //init RecyclerViews
+        this.initRecyclerView()
     }
 
     override fun onBackPressed() {
