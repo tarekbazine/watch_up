73e755bc102b7a420a5e55bf81d3f676ca138899
terryBaz <et_bazine@esi.dz>
2018-03-26 23:30:22 +0100

    create film card

--- app/src/main/java/com/example/tarekbaz/watch_up/CinemaActivity.kt
+++ app/src/main/java/com/example/tarekbaz/watch_up/CinemaActivity.kt
@@ -98,7 +98,7 @@ class CinemaActivity : AppCompatActivity() {
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                                   savedInstanceState: Bundle?): View? {
             val rootView = inflater.inflate(R.layout.fragment_film, container, false)
-            rootView.section_label.text = getString(R.string.section_format, arguments.getInt(ARG_SECTION_NUMBER))
+//            rootView.section_label.text = getString(R.string.section_format, arguments.getInt(ARG_SECTION_NUMBER))
             return rootView
         }
 
@@ -132,7 +132,7 @@ class CinemaActivity : AppCompatActivity() {
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                                   savedInstanceState: Bundle?): View? {
             val rootView = inflater.inflate(R.layout.fragment_film, container, false)
-            rootView.section_label.text = getString(R.string.section_format, arguments.getInt(ARG_SECTION_NUMBER))
+//            rootView.section_label.text = getString(R.string.section_format, arguments.getInt(ARG_SECTION_NUMBER))
             return rootView
         }
 
