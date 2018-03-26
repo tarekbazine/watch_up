9f852a595e515960f70983d3cd56217961b0d6e4
mahfoud <mail@esi.com>
2018-03-26 12:31:15 +0100

    viewPager + home_drawer

---
+++ app/src/main/java/com/example/tarekbaz/watch_up/HomeActivity.kt
@@ -0,0 +1,63 @@
+package com.example.tarekbaz.watch_up
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.app_bar_home.*
+
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_home -> {
+                // Handle the camera action
+            }
+            R.id.nav_cinema -> {
+
+            }
+            R.id.nav_series -> {
+
+            }
+            R.id.nav_personnes -> {
+
+            }
+            R.id.nav_fan -> {
+
+            }
+            R.id.nav_evaluations -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
--- app/src/main/java/com/example/tarekbaz/watch_up/Models/Mocker.kt
+++ app/src/main/java/com/example/tarekbaz/watch_up/Models/Mocker.kt
@@ -4,7 +4,7 @@ package com.example.tarekbaz.watch_up.Models
 object Mocker{
     init {
         val MovieList: List<Movie> = mutableListOf(
-                Movie("now you see it" , "desc",0, null,null,null,null )
+//                Movie("now you see it" , "desc",0, null,null,null,null )
         )
 
     }
